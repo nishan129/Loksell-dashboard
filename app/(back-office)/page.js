b1c1a9614086e7e344b8995dbd708ab924fed80d
nishant129 <nishantborkar139@gmail.com>
2024-08-02 13:02:11 +0530

    update the page

--- app/(back-office)/page.js
+++ app/(back-office)/page.js
@@ -1,40 +1,9 @@
-import Heading from '@/components/backoffice/Heading';
-import LargeCards from '@/components/backoffice/LargeCards';
-import SmallCards from '@/components/backoffice/SmallCards';
-import DashboardCharts from '@/components/backoffice/DashboardCharts';
-import React from 'react';
-import CustomDataTable from '@/components/backoffice/CustomDataTable';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/authOptions';
-import KiranaDashboard from '@/components/backoffice/KiranaDashboard';
-import WholesalerDashgboard from '@/components/backoffice/WholesalerDashgboard';
-import { getData } from '@/lib/getData';
+import Image from "next/image";
 
-export default async function page() {
-  const session = await getServerSession(authOptions)
-  const role  = session?.user?.role;
-  const sales = await getData("sale")
-  const products = await getData("products");
-  const orders  = await getData("orders");
-  
-  
-  if(role==="KIRANA"){
-    return <KiranaDashboard />;
-  }
-  if (role==="WHOLESALER"){
-    return <WholesalerDashgboard />;
-  }
+export default function Home() {
   return (
-    <div>
-        <Heading title="Dashboard Overview"/>
-        {/* Large Cards */}
-        <LargeCards sales={sales} />
-        {/* Small Cards */}
-        <SmallCards  orders={orders}/>
-        {/* Charts */}
-        <DashboardCharts sales={sales} orders={orders}/>
-        {/* Recent Orders Table */}
-        {/* <CustomDataTable />  */}
+    <div className="flex items-center justify-center flex-col min-h-screen">
+      <h2 className="text-4xl">Dashboard</h2>
     </div>
   );
 }
