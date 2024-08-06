ee8030b7e20ef08e3b69bbc58ac2757e4e26f0b3
nishant129 <nishantborkar139@gmail.com>
2024-08-07 01:24:59 +0530

    update the sale route

--- app/api/sale/route.js
+++ app/api/sale/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const sales = await db.sale.findMany({
+    const sales = await db.Sale.findMany({
       orderBy: {
         createdAt: "desc",
       },
