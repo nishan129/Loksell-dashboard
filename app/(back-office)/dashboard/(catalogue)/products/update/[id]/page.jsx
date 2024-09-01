d1b581290788a3858c3c6028e86901625d8f26d8
nishant129 <nishantborkar139@gmail.com>
2024-09-01 22:09:17 +0530

    update category

--- app/(back-office)/dashboard/(catalogue)/products/update/[id]/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/products/update/[id]/page.jsx
@@ -2,26 +2,31 @@ import React from 'react';
 import FormHeader from '@/components/backoffice/FormHeader';
 import NewProductsForm from '@/components/backoffice/NewProductForm';
 import { getData } from '@/lib/getData';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/lib/authOptions';
 
 export default async function UpdateProduct({ params: { id } }) {
-    // Fetch categories and users data
-    const categoriesData = await getData("categories")
-    const usersData = await getData("users")
-    const product = await getData(`products/${id}`)
-    // Filter and map wholesalers
-    const wholesaller = usersData
-        .filter(user => user.role === "WHOLESALER")
-        .map(user => ({ id: user.id, title: user.name }));
+    // Fetch data for categories, users, and the specific product
+    const [categoriesData, usersData, product] = await Promise.all([
+        getData("categories"),
+        getData("users"),
+        getData(`products/${id}`)
+    ]);
 
-    // Map categories
-    const categories = categoriesData.map(category => ({ id: category.id, title: category.title }));
+    // Extract and map wholesaler data
+    const wholesalers = usersData
+        .filter(user => user.role === "WHOLESALER")
+        .map(({ id, name }) => ({ id, title: name }));
+
+    // Map categories data
+    const categories = categoriesData.map(({ id, title }) => ({ id, title }));
 
     return (
         <div>
             <FormHeader title="Update Product" />
-            <NewProductsForm categories={categories} wholesaller={wholesaller} updateData={product} />
+            <NewProductsForm 
+                categories={categories} 
+                wholesalers={wholesalers} 
+                updateData={product} 
+            />
         </div>
     );
 }
